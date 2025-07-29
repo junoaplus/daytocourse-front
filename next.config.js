095671a69ea21a3f011df0b7dca9f33efd47d250
junoaplus <junehoo4248@naver.com>
2025-07-29 19:32:17 +0900

    env

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'https://es1bdttovetr1c-8000.proxy.runpod.net'}/:path*`
       }
     ];
   },
